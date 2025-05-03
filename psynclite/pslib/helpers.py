fecdbe5938ad64cd6d828736d44a8fbc3549bb99
ScorpiusRL <didyka38@gmail.com>
2025-05-03 19:44:38 +0300

    The argument system has been improved. Update README file.

--- psynclite/pslib/helpers.py
+++ psynclite/pslib/helpers.py
@@ -23,7 +23,6 @@ def helpSynclite():
     {COLORS['yellow']}----------- Synclite is the main control system ----------{COLORS['reset']}
 
     {COLORS['green']}-h --help         			               Show this message{COLORS['reset']}
-    {COLORS['green']}-i --init         			               Initialization PSynclite{COLORS['reset']}
 
     {COLORS['yellow']}---------- System and its properties ----------{COLORS['reset']}
 
@@ -36,16 +35,21 @@ def helpSynclite():
 
     {COLORS['yellow']}------- Volume and Brightness Control ---------{COLORS['reset']}
 
-    {COLORS['cyan']}-vi --vol-inc{COLORS['reset']}             Increment volume (+5%)
-    {COLORS['cyan']}-vd --vol-dec{COLORS['reset']}             Decrement volume (-5%)
-    {COLORS['cyan']}-bi --bri-inc{COLORS['reset']}             Increment Brightness (+5%)
-    {COLORS['cyan']}-bd --bri-dec{COLORS['reset']}             Decrement Brightness (-5%)
+    {COLORS['cyan']}-V inc{COLORS['reset']}                    Increment volume (+5%)
+    {COLORS['cyan']}   dec{COLORS['reset']}                    Decrement volume (-5%)
+    {COLORS['cyan']}-B inc{COLORS['reset']}                    Increment Brightness (+5%)
+    {COLORS['cyan']}   dec{COLORS['reset']}                    Decrement Brightness (-5%)
 
     {COLORS['yellow']}--------- Media and System Control ------------{COLORS['reset']}
 
-    {COLORS['cyan']}-mpp --media-pp{COLORS['reset']}           Control media (play/pause)
-    {COLORS['cyan']}-mpn --media-pn{COLORS['reset']}           Next song
-    {COLORS['cyan']}-mppr --media-ppr{COLORS['reset']}         Previous song
+    {COLORS['cyan']}-M player toggle{COLORS['reset']}          Control media (play/pause)
+    {COLORS['cyan']}-M player next{COLORS['reset']}            Next song
+    {COLORS['cyan']}-M player previous{COLORS['reset']}        Previous song
+    {COLORS['cyan']}-M duration minsec{COLORS['reset']}        Player's duration in min:sec
+    {COLORS['cyan']}-M duration sec{COLORS['reset']}           Player's duration in seconds
+    {COLORS['cyan']}-M position minsec{COLORS['reset']}        Player's position in min:sec
+    {COLORS['cyan']}-M position sec{COLORS['reset']}           Player's position in seconds
+
     {COLORS['cyan']}-wp --wallpaper{COLORS['reset']}           Set random wallpaper
     {COLORS['cyan']}-ps --printscreen{COLORS['reset']}         Make screen shot
 
